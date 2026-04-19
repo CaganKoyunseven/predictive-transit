f80611b47a46b0bd76cbc4fb2cf3cc42b097cd5c
CaganKoyunseven <koyunsevencagan@gmail.com>
2026-04-19 22:24:22 +0300

    feat: predefined Sivas POI list for place search

---
+++ frontend/src/data/places.ts
@@ -0,0 +1,24 @@
+export interface Place {
+  name: string
+  lat: number
+  lng: number
+}
+
+export const PLACES: Place[] = [
+  { name: "Cumhuriyet Üniversitesi", lat: 39.748, lng: 36.978 },
+  { name: "Sivas Üniversitesi",      lat: 39.748, lng: 36.978 },
+  { name: "Üniversite Kampüsü",      lat: 39.615, lng: 37.063 },
+  { name: "Üniversite Yurdu",        lat: 39.615, lng: 37.063 },
+  { name: "Sivas Devlet Hastanesi",  lat: 39.757, lng: 36.977 },
+  { name: "Numune Hastanesi",        lat: 39.789, lng: 37.067 },
+  { name: "Sağlık Merkezi",          lat: 39.790, lng: 37.061 },
+  { name: "Otogar",                  lat: 39.761, lng: 36.978 },
+  { name: "Terminal",                lat: 39.623, lng: 37.095 },
+  { name: "Tren Garı",               lat: 39.624, lng: 37.061 },
+  { name: "Şehir Merkezi",           lat: 39.748, lng: 37.014 },
+  { name: "Meydan",                  lat: 39.748, lng: 37.014 },
+  { name: "Sanayi Çarşısı",          lat: 39.777, lng: 37.076 },
+  { name: "Pazar Yeri",              lat: 39.799, lng: 37.051 },
+  { name: "Bağlar",                  lat: 39.762, lng: 37.007 },
+  { name: "Esentepe",                lat: 39.761, lng: 36.978 },
+]
